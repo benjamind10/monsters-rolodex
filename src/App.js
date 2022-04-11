9e118b42b7fd4e062f52ab3aca45215a2a11b816
Benjamin Duran <benjamind10@pm.me>
2022-04-11 14:46:01 -0400

    linked react app & created fetch call for users

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    console.log('constructor');
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log('component did mount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
